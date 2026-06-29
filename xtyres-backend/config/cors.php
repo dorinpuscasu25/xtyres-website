fb5331aa10d2b2074e1a4ce4fb50be6426eb631b
Dorin Puscasu <46497424+dorinpuscasu25@users.noreply.github.com>
2026-06-29 23:40:31 +0300

    update

--- xtyres-backend/config/cors.php
+++ xtyres-backend/config/cors.php
@@ -5,6 +5,8 @@ $defaultOrigins = [
     'http://localhost:5173',
     'http://127.0.0.1:5174',
     'http://localhost:5174',
+    'https://xtyres.md',
+    'https://www.xtyres.md',
     'https://new.xtyres.md',
     'new.xtyres.md'
 ];
